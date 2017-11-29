7991209253beced0ae5e1b3ebed6cdae25ba10e0
Shitikov Kyrylo <kshy@isddesign.com>
2017-11-29 23:21:28 +0200

    Realise admin page

--- ember/app/models/author.js
+++ ember/app/models/author.js
@@ -1,7 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  char: DS.attr(),
   name: DS.attr(),
   surname: DS.attr(),
+  'char-en': DS.attr(),
+  'name-en': DS.attr(),
+  'surname-en': DS.attr(),
   biography: DS.attr()
 });
