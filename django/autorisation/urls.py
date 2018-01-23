a1a60c3fafdbff615c8651e683718969215f0ec1
Shitikov Kyrylo <kshy@isddesign.com>
2018-01-23 23:24:14 +0200

    [DJNG-2] User API

--- django/autorisation/urls.py
+++ django/autorisation/urls.py
@@ -1,6 +1,12 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from .views import *
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'groups', GroupViewSet)
 
 urlpatterns = [
+    url(r'^', include(router.urls)),
     url(r'^$', custom_auth_token),
 ]
